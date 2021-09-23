cbdd3795ff0c996287cdadf7e3efd21fbb4981f4
Bárbara Ann Lima <babiplima@gmail.com>
2021-09-23 14:36:47 -0300

    Oitavo commit

--- src/Inimigos/InimigosRasteiros.java
+++ src/Inimigos/InimigosRasteiros.java
@@ -3,4 +3,31 @@ package Inimigos;
 public class InimigosRasteiros extends Inimigos {
     private double danoEspinhos;
     private double aplicarDano;
+
+    public InimigosRasteiros(double danoEspinhos, double aplicarDano) {
+        this.danoEspinhos = danoEspinhos;
+        this.aplicarDano = aplicarDano;
+    }
+
+    public InimigosRasteiros(String nome, double vida, double danoEspinhos, double aplicarDano) {
+        super(nome, vida);
+        this.danoEspinhos = danoEspinhos;
+        this.aplicarDano = aplicarDano;
+    }
+
+    public double getDanoEspinhos() {
+        return danoEspinhos;
+    }
+
+    public void setDanoEspinhos(double danoEspinhos) {
+        this.danoEspinhos = danoEspinhos;
+    }
+
+    public double getAplicarDano() {
+        return aplicarDano;
+    }
+
+    public void setAplicarDano(double aplicarDano) {
+        this.aplicarDano = aplicarDano;
+    }
 }
--- src/Inimigos/InimigosVoadores.java
+++ src/Inimigos/InimigosVoadores.java
@@ -3,4 +3,31 @@ package Inimigos;
 public class InimigosVoadores extends Inimigos {
     private double danoTiro;
     private double aplicarDano;
+
+    public InimigosVoadores(double danoTiro, double aplicarDano) {
+        this.danoTiro = danoTiro;
+        this.aplicarDano = aplicarDano;
+    }
+
+    public InimigosVoadores(String nome, double vida, double danoTiro, double aplicarDano) {
+        super(nome, vida);
+        this.danoTiro = danoTiro;
+        this.aplicarDano = aplicarDano;
+    }
+
+    public double getDanoTiro() {
+        return danoTiro;
+    }
+
+    public void setDanoTiro(double danoTiro) {
+        this.danoTiro = danoTiro;
+    }
+
+    public double getAplicarDano() {
+        return aplicarDano;
+    }
+
+    public void setAplicarDano(double aplicarDano) {
+        this.aplicarDano = aplicarDano;
+    }
 }
