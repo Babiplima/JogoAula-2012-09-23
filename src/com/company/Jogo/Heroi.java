af143669836efbcf758cec4f2d1e2c8021542e7d
Bárbara Ann Lima <babiplima@gmail.com>
2021-09-23 11:57:32 -0300

    Primeiro commit

---
+++ src/com/company/Jogo/Heroi.java
@@ -0,0 +1,29 @@
+package com.company.Jogo;
+
+public class Heroi {
+    private String nome;
+    private double vida;
+
+    public Heroi(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    // Método trocar
+
+    public void trocar(String trocarNome) {
+        trocarNome = nome;
+    }
+    //Receber dano
+
+    public void receber(double receberDano) {
+        receberDano = vida;
+
+        //Exibir vida
+
+        public void exibir ( double exibirVida){
+            exibirVida = vida;
+        }
+
+    }
+}
---
+++ src/com/company/Jogo/Main.java
@@ -0,0 +1,10 @@
+package com.company.Jogo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
