a52664dd280462a0e642553aacbc612d62c02468
Bárbara Ann Lima <babiplima@gmail.com>
2021-09-23 21:44:29 -0300

    Décimo commit

--- src/com/company/Jogo/Main.java
+++ src/com/company/Jogo/Main.java
@@ -1,14 +1,12 @@
 package com.company.Jogo;
 
-import Inimigos.Inimigos;
-
 public class Main {
 
     public static void main(String[] args) {
         Magos mago = new Magos("Mestre dos Magos", 10, 30);
         Guerreiros guerreiro = new Guerreiros("Thor", 300, 500);
         Inimigos inimigos = new Inimigos("Lok", 400);
-        Inimigos.InimigosRasteiros("Porco espinho",300,300);
+        InimigosRasteiros inimigosRateiros = new InimigosRasteiros("Porco espinho",300,300);
 
     }
 }
